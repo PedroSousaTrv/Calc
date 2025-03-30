9f346e47ba7a24b246c6706a80e808eb4b7f4504
Pedro  de Sousa <pedrosousa2003@gmail.com>
2025-03-30 16:39:20 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const display = document.getElementById("resultado")
+
+function insert(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error."
+    }
+}
